02bf0a4744eec882b8898107f55e82cef4d7191e
Aleksandrs Zeltisevs <azheltishevs@gmail.com>
2023-06-04 15:06:37 +0300

    add random images

--- Api/Services/Helpers/MapperHelper.cs
+++ Api/Services/Helpers/MapperHelper.cs
@@ -5,6 +5,17 @@ namespace Api.Services.Helpers
 {
     public static class MapperHelper
     {
+        private static readonly List<String> schoolImageUrls = new()
+        {
+            "https://i.ibb.co/vxrHFyn/1.jpg",
+            "https://i.ibb.co/smdtBsS/2.jpg",
+            "https://i.ibb.co/MC10KLw/3.jpg",
+            "https://i.ibb.co/GtLKw4B/4.jpg",
+            "https://i.ibb.co/16xGK7w/5.jpg",
+            "https://i.ibb.co/7j8BDfY/6.jpg",
+            "https://i.ibb.co/BPf4YhG/7.jpg"
+        };
+
         public static SingleSchoolView Map(int index, HighSchool from)
         {
             return new SingleSchoolView()
@@ -14,8 +25,9 @@ namespace Api.Services.Helpers
                 City = from.Municipality,
                 TotalNumberOfStudents = from.TotalNumberOfStudents,
                 ImageUrl =
-                    "https://www.greatschools.org/gk/wp-content/uploads/2014/03/The-school-visit-what-to-look-for-what-to-ask-1-750x325.jpg",
-                Description = $@"Welcome to {from.NameOfInstitution}, a vibrant and inclusive educational institution nestled in the heart of a bustling suburban town. With a rich history spanning over six decades, {from.NameOfInstitution} has established itself as a beacon of academic excellence, fostering a nurturing environment where students can thrive and reach their full potential.
+                    schoolImageUrls[Faker.RandomNumber.Next(schoolImageUrls.Capacity)],
+                Description =
+                    $@"Welcome to {from.NameOfInstitution}, a vibrant and inclusive educational institution nestled in the heart of a bustling suburban town. With a rich history spanning over six decades, {from.NameOfInstitution} has established itself as a beacon of academic excellence, fostering a nurturing environment where students can thrive and reach their full potential.
 
 As you step onto the meticulously manicured campus, you'll be greeted by the sight of a modern, purpose-built facility surrounded by lush greenery and flourishing gardens. The school's architecture seamlessly blends contemporary design with elements of traditional charm, creating a visually stunning atmosphere that inspires creativity and curiosity.",
                 ContactPerson = new ContactPerson()
